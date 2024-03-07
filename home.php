913069384b1509f134b59551367251a6134e0863
“gabi” <plataramirez17@gmail.com>
2024-03-07 20:24:18 -0300

    creo header

---
+++ home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lab Dental Congreso</title>
+    <link rel="icon" href="images/logo.png">
+    <style>
+        * {
+            background-color: #C7EAFF;
+        }
+
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .icon {
+            width: 45px;
+            height: auto;
+            margin-right: 5px;
+        }
+
+        .logo {
+            width: 170px;
+            height: auto;
+        }
+
+        .sec-ubicacion {
+            display: flex;
+            align-items: center;
+        }
+
+        .sec-ubicacion p {
+            margin-left: 5px;
+        }
+
+        .icons-social {
+            display: flex;
+            align-items: center;
+        }
+
+        .icons-social p {
+            margin-left: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <nav class="sec-ubicacion">
+            <img src="images/ubicacion.png" alt="" class="icon">
+            <p>Adolfo Alsina 1609, Piso 7 Oficina 7 <br> Monserrat - Capital Federal</p>
+        </nav>
+        <a href="home.php"><img src="images/logo.png" alt="" class="logo"></a>
+        <nav class="icons-social">
+            <a href=""><img src="images/whatsapp.png" alt="" class="icon"></a>
+            <a href=""><img src="images/facebook.png" alt="" class="icon"></a>
+            <a href=""><img src="images/instagram.png" alt="" class="icon"></a>
+            <a href=""><img src="images/correo.png" alt="" class="icon"></a>
+            <a href=""><img src="images/telefono.png" alt="" class="icon"></a>
+            <p>11 5601-8912</p>
+        </nav>
+    </header>
+</body>
+
+</html>
