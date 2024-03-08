d3e4b79f6847fbc24b6062606a1f0f43a1deb918
“gabi” <plataramirez17@gmail.com>
2024-03-08 00:37:06 -0300

    añado portada

--- home.php
+++ home.php
@@ -5,19 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lab Dental Congreso</title>
     <link rel="icon" href="images/logo.png">
+
     <style>
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
+
         * {
-            background-color: #C7EAFF;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
         }
 
         header {
+            background-color: #fff;
+            padding: 0 10px;
             display: flex;
             justify-content: space-between;
             align-items: center;
         }
 
         .icon {
-            width: 45px;
+            width: 40px;
             height: auto;
             margin-right: 5px;
         }
@@ -33,7 +41,9 @@
         }
 
         .sec-ubicacion p {
+            font-size: 17px;
             margin-left: 5px;
+            color: #004AAD;
         }
 
         .icons-social {
@@ -42,27 +52,60 @@
         }
 
         .icons-social p {
+            font-size: 17px;
             margin-left: 5px;
+            color: #004AAD;
+        }
+
+        .portada img {
+            width: 100%;
+            height: auto;
+        }
+
+        main {
+            background-color: #C7EAFF;
+        }
+
+        .text-portada {
+            position: absolute;
+            top: 80%;
+            left: 5%;
+            background-color: #C7EAFF;
+            opacity: 0.8;
+            padding: 30px;
         }
     </style>
 </head>
 
 <body>
     <header>
-        <nav class="sec-ubicacion">
+        <section class="sec-ubicacion">
             <img src="images/ubicacion.png" alt="" class="icon">
-            <p>Adolfo Alsina 1609, Piso 7 Oficina 7 <br> Monserrat - Capital Federal</p>
-        </nav>
+            <i>
+                <p>Adolfo Alsina 1609, Piso 7 Oficina 7 <br> Monserrat - Capital Federal</p>
+            </i>
+        </section>
         <a href="home.php"><img src="images/logo.png" alt="" class="logo"></a>
-        <nav class="icons-social">
+        <section class="icons-social">
             <a href=""><img src="images/whatsapp.png" alt="" class="icon"></a>
             <a href=""><img src="images/facebook.png" alt="" class="icon"></a>
             <a href=""><img src="images/instagram.png" alt="" class="icon"></a>
             <a href=""><img src="images/correo.png" alt="" class="icon"></a>
             <a href=""><img src="images/telefono.png" alt="" class="icon"></a>
             <p>11 5601-8912</p>
-        </nav>
+        </section>
     </header>
+
+    <main>
+        <div class="portada">
+            <img src="images/portada.png" alt="">
+            <div class="text-portada">
+                <h3>¡Bienvenidos a LabDental Congreso!</h3>
+                <p> Somos especialistas en la fabricación y reparación de prótesis removibles. <br> Con nosotros, encontrarás soluciones dentales confiables y de calidad</p>
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
